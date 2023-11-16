1dcc343bac5e4279001fc7d86a38495d2529ee44
Natyren <bredis.georgy@yandex.ru>
2023-11-16 22:08:16 +0300

    Add test version of HomoContrastive

--- model/utils.py
+++ model/utils.py
@@ -4,7 +4,69 @@ import numpy as np
 
 
 class HomoContrastive(nn.Module):
-    pass
+    def __init__(
+        self, temperature=0.05, contrast_mode="all", base_temperature=0.07
+    ):
+        super().__init__()
+        self.temperature = temperature
+        self.contrast_mode = contrast_mode
+        self.base_temperature = base_temperature
+
+    def forward(self, features, labels):
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+
+        total_loss = 0
+        for _labels, item in zip(labels, features):
+            labels = _labels[:, 0].view(-1, 1)
+            batch_size = item.shape[0]
+            mask = torch.eq(labels, labels.T).float().to(device)
+
+            contrast_count = item.shape[1]
+            contrast_feature = torch.cat(torch.unbind(item, dim=1), dim=0)
+            if self.contrast_mode == "one":
+                anchor_feature = item[:, 0]
+                anchor_count = 1
+            elif self.contrast_mode == "all":
+                anchor_feature = contrast_feature
+                anchor_count = contrast_count
+            else:
+                raise ValueError("Unknown mode: {}".format(self.contrast_mode))
+
+            # compute logits
+            anchor_dot_contrast = torch.div(
+                torch.matmul(anchor_feature, contrast_feature.T),
+                self.temperature,
+            )
+            # for numerical stability
+            logits_max, _ = torch.max(anchor_dot_contrast, dim=1, keepdim=True)
+            logits = anchor_dot_contrast - logits_max.detach()
+
+            # tile mask
+            mask = mask.repeat(anchor_count, contrast_count)
+            # mask-out self-contrast cases
+            logits_mask = torch.scatter(
+                torch.ones_like(mask),
+                1,
+                torch.arange(batch_size * anchor_count).view(-1, 1).to(device),
+                0,
+            )
+            mask = mask * logits_mask
+
+            # compute log_prob
+            exp_logits = torch.exp(logits) * logits_mask
+            log_prob = logits - torch.log(exp_logits.sum(1, keepdim=True))
+
+            # compute mean of log-likelihood over positive
+            mean_log_prob_pos = (mask * log_prob).sum(1) / mask.sum(1)
+
+            # loss
+            loss = (
+                -(self.temperature / self.base_temperature) * mean_log_prob_pos
+            )
+            loss = loss.view(anchor_count, batch_size).mean()
+            total_loss += loss
+
+        return total_loss
 
 
 def mixing(
