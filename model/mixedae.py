75d6f4d7c08e3804018b59f9be2c1992ee1b1b7e
Natyren <bredis.georgy@yandex.ru>
2023-11-18 16:20:46 +0300

    add homocontrast to forward pass

--- model/mixedae.py
+++ model/mixedae.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 
 # from .utils import get_2d_sincos_pos_embed
 from .modeling import PatchEmbed, Block
-from .utils import mixing
+from .utils import mixing, HomoContrastive, get_2d_sincos_pos_embed
 
 
 class MixedMaskedAutoencoderViT(nn.Module):
@@ -22,6 +22,7 @@ class MixedMaskedAutoencoderViT(nn.Module):
         decoder_num_heads=16,
         mlp_ratio=4.0,
         norm_layer=nn.LayerNorm,
+        temperature=0.5,
         norm_pix_loss=False,
     ):
         super().__init__()
@@ -78,6 +79,53 @@ class MixedMaskedAutoencoderViT(nn.Module):
         )
 
         self.norm_pix_loss = norm_pix_loss
+
+        self.contrastive_loss = HomoContrastive()
+        self.t = temperature
+
+        self.initialize_weights()
+
+    def initialize_weights(self):
+        # initialization
+        # initialize (and freeze) pos_embed by sin-cos embedding
+        pos_embed = get_2d_sincos_pos_embed(
+            self.pos_embed.shape[-1],
+            int(self.patch_embed.num_patches**0.5),
+            cls_token=True,
+        )
+        self.pos_embed.data.copy_(
+            torch.from_numpy(pos_embed).float().unsqueeze(0)
+        )
+
+        decoder_pos_embed = get_2d_sincos_pos_embed(
+            self.decoder_pos_embed.shape[-1],
+            int(self.patch_embed.num_patches**0.5),
+            cls_token=True,
+        )
+        self.decoder_pos_embed.data.copy_(
+            torch.from_numpy(decoder_pos_embed).float().unsqueeze(0)
+        )
+
+        # initialize patch_embed like nn.Linear (instead of nn.Conv2d)
+        w = self.patch_embed.proj.weight.data
+        torch.nn.init.xavier_uniform_(w.view([w.shape[0], -1]))
+
+        # timm's trunc_normal_(std=.02) is effectively normal_(std=0.02) as cutoff is too big (2.)
+        torch.nn.init.normal_(self.cls_token, std=0.02)
+        torch.nn.init.normal_(self.mask_token, std=0.02)
+
+        # initialize nn.Linear and nn.LayerNorm
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            # we use xavier_uniform following official JAX ViT:
+            torch.nn.init.xavier_uniform_(m.weight)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
 
     def shuffling(self, x, n_splits=4):
         mixed = [
@@ -163,4 +211,5 @@ class MixedMaskedAutoencoderViT(nn.Module):
         latent, ids = self.forward_encoder(x)
         pred = self.forward_decoder(latent, ids)
         loss = self.forward_loss(x, pred)
+        loss += self.t * self.contrastive_loss(latent[:, 1:, :], ids)
         return loss, pred
